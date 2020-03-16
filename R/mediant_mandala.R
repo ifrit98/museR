08ccaf803aa2c6d8ccb2349137991aaa7776548d
JasonStGeorge <ifrit98@gmail.com>
2020-03-16 18:22:05 -0400

    mediant mandala added

---
+++ R/mediant_mandala.R
@@ -0,0 +1,8 @@
+mediant_mandala <- function() {
+
+  tw  <- c('C', 'C#', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B')
+  pm  <- c('a', 'bb', 'b', 'c', 'c#', 'd', 'eb', 'e', 'f', 'f#', 'g', 'ab')
+  cpm <- c('e', 'f', 'f#', 'g', 'ab', 'a', 'bb', 'b', 'c', 'c#', 'd', 'eb')
+
+
+}
